16b88b7b8f83c9856b3f29a3cef931c4e7f43fcc
Sckab <giulianodeamicis2009@gmail.com>
2025-11-26 00:41:32 +0100

    removed useless/unused aliases, re-ordered them in categories

--- .zsh/aliases-functions.zsh
+++ .zsh/aliases-functions.zsh
@@ -18,76 +18,87 @@ function cr_fn()
 precmd_functions+=(set_cursor_beam)
 
 # ====== ALIAS ====== #
-alias ls='eza -lh'
-alias lsa='eza -alh'
-alias ln='eza --icons'
-alias lna='eza -a --icons'
+# DIRECTORIES AND FILES
+alias p='pwd'
 alias c='clear'
-alias grep='grep --color=auto'
-alias clock='tty-clock -c -C 2 -s -S'
-alias nf='neofetch'
-alias ff='fastfetch'
-alias pipes='pipes.sh -c2 -t1'
-alias exa='exa --icons'
-alias speedtest='speedtest-cli'
-alias stupiscimi='fortune | cowsay | lolcat'
-alias u='yay -Syu'
-alias rp='sudo pacman -Rns'
-alias ra='yay -Rns'
-alias sus='cat ~/.config/ascii_arts/among_us.txt'
-alias susc='cat ~/.config/ascii_arts/among_us.txt | lolcat'
-alias nivm='nvim'
-alias sharp-console='dotnet new console -n'
-alias zo='source ~/.zshrc'
-alias n='nvim'
-alias russian-roulette='.config/scripts/russian_roulette.sh'
-alias t='tmux'
-alias tk='tmux kill-server'
-alias e='exit'
-alias r='ranger'
-alias l='lazygit'
-alias giulsnet='sudo ~/.config/scripts/giulsnet.sh'
 alias cr='cr_fn'
 alias ncd='ncdu'
-alias please='sudo'
-alias ze='zellij'
-alias kanagawa='~/.config/scripts/kanagawa_palette.sh'
-alias py='python3'
-alias pin='ping 8.8.8.8'
-alias gpi='gping 8.8.8.8'
-alias mpi='~/.config/scripts/megaping.py'
-alias mpi-google='mpi --host google.com --count 3 --timeout 1 --size 64'
-alias cn='clear && nvim'
+alias r='ranger'
+alias ls='eza -lh'
+alias rm='rm -rfv'
+alias ae="chmod +x"
+alias lsa='eza -alh'
+alias ln='eza --icons'
 alias ce='clear && exa'
+alias exa='exa --icons'
+alias cn='clear && nvim'
+alias lna='eza -a --icons'
 alias cl='clear && lazygit'
 alias cne='clear && neofetch'
 alias cff='clear && fastfetch'
-alias zc='source ~/.zshrc && clear'
-alias ai-code='ollama run deepseek-coder'
-alias ai-chat='ollama run llama3:8b'
+alias grep='grep --color=auto'
 alias cld='clear && lazydocker'
-alias car='cargo run'
-alias cac='cargo check'
-alias cab='cargo build'
-alias p='pwd'
-alias gitcleanall='git fetch --prune && git remote prune origin && git gc --aggressive --prune=now'
-alias my='shutdown now'
-alias nc='mpd && ncmpcpp'
-alias la='NVIM_APPNAME=nvim-lazyvim nvim'
-alias nde='NVIM_APPNAME=nvim-nde nvim'
-alias nv='NVIM_APPNAME=nvchad nvim'
-alias Dr='dotnet run'
-alias Drw='dotnet watch run'
 alias pj='. ~/.config/scripts/projects.sh'
 alias tr="tree . -I 'node_modules|.next|.nuxt|dist|out|.cache|bin|obj|TestResults|__pycache__|venv|.idea|.vscode|.git' --dirsfirst -C"
-alias rm='rm -rfv'
-alias isaac='/home/giuliano/.config/scripts/run_isaac.sh'
-alias hypr='cd /home/giuliano/.config/hypr'
-alias ur='ga README.md && gc -m "update README" && gp'
-alias ib='imv -b 181616'
-alias i='imv'
+
+# NVIM
+alias n='nvim'
+alias nv='NVIM_APPNAME=nvchad nvim'
+alias nde='NVIM_APPNAME=nvim-nde nvim'
+alias la='NVIM_APPNAME=nvim-lazyvim nvim'
 alias nm='NVIM_APPNAME=nvim-minimal nvim'
+
+# MUSIC
+alias nc='mpd && ncmpcpp'
+
+# GIT
+alias l='lazygit'
+alias ur='ga README.md && gc -m "update README" && gp'
+alias gitcleanall='git fetch --prune && git remote prune origin && git gc --aggressive --prune=now'
+
+# DEV
+alias m="make"
+alias py='python3'
+alias car='cargo run'
+alias Dr='dotnet run'
+alias cab='cargo build'
+alias cac='cargo check'
+alias Drw='dotnet watch run'
+alias sharp-console='dotnet new console -n'
+
+# INTERNET
+alias pin='ping 8.8.8.8'
+alias gpi='gping 8.8.8.8'
+alias mpi-google='mpi --host google.com --count 3 --timeout 1 --size 64'
+
+# TMUX
+alias e='exit'
+alias t='tmux'
+alias tk='tmux kill-server'
+
+# AI
+alias ai-chat='ollama run llama3:8b'
+alias ai-code='ollama run deepseek-coder'
+
+# UTILITY
+alias i='imv'
+alias please='sudo'
+alias ra='yay -Rns'
+alias my='shutdown now'
+alias ib='imv -b 181616'
+alias rp='sudo pacman -Rns'
+alias zc='source ~/.zshrc && clear'
 alias u="~/.config/scripts/utility.sh"
 alias pi="~/.config/scripts/project-initter/main.sh"
-alias m="make"
-alias ae="chmod +x"
+
+# OTHERS
+alias nf='neofetch'
+alias ff='fastfetch'
+alias pipes='pipes.sh -c2 -t1'
+alias speedtest='speedtest-cli'
+alias clock='tty-clock -c -C 2 -s -S'
+alias stupiscimi='fortune | cowsay | lolcat'
+alias sus='cat ~/.config/ascii_arts/among_us.txt'
+alias kanagawa='~/.config/scripts/kanagawa_palette.sh'
+alias susc='cat ~/.config/ascii_arts/among_us.txt | lolcat'
+alias russian-roulette='.config/scripts/russian_roulette.sh'
