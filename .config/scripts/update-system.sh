297f10aec23b5a45feb25cfaf7254d2d3b57dd14
Sckab <giulianodeamicis2009@gmail.com>
2026-01-14 21:09:52 +0100

    update to the general config, added a script that runs on ghostty start-up and updated the fastfetch config

---
+++ .config/scripts/update-system.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+export GUM_SPIN_SPINNER_FOREGROUND="#76946A"
+
+fastfetch
+
+echo
+
+ask_update() {
+    gum confirm \
+        --prompt.foreground="#76946A" \
+        --selected.foreground="#2A2A2A" \
+        --selected.background="#76946A" \
+        --unselected.foreground="#76946A" \
+        --unselected.background="#2A2A2A" \
+        "You want to update the system?" || return
+
+    yay -Syu
+}
+
+UPDATES_AUR=$(gum spin --spinner dot --title "Getting AUR updates..." -- yay -Qu | wc -l)
+UPDATES_PACMAN=$(gum spin --spinner dot --title "Getting pacman updates..." -- pacman -Qu | wc -l)
+
+TOTAL_UPDATES=$((UPDATES_AUR + UPDATES_PACMAN))
+
+dashes=$(echo "Total  -> $TOTAL_UPDATES" | sed "s/./-/g")
+
+echo -e "\e[32mUPDATES\e[0m"
+echo -e "\e[32m$dashes\e[0m"
+echo -e "\e[32mPacman -> \e[0m$UPDATES_PACMAN"
+echo -e "\e[32mAUR    -> \e[0m$UPDATES_AUR"
+echo -e "\e[32m$dashes\e[0m"
+echo -e "\e[32mTotal  -> \e[0m$TOTAL_UPDATES"
+
+echo
+
+ask_update
+
+tmux new-session
